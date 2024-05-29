19bd1be6ad06e82753d02488921eefe94f10846a
안영진 <158057603+janyoungjin@users.noreply.github.com>
2024-05-29 15:28:26 +0900

    create requirements.txt

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 from fastapi import FastAPI, Form
-from fastapi.responses import FileResponse,JSONResponse
+from fastapi.responses import HTMLResponse,JSONResponse
 import uvicorn
 
 app = FastAPI()
 
 # 데이터 로드 및 KNN 모델 학습
-df = pd.read_csv("https://raw.githubusercontent.com/janyoungjin/AIDATA/main/data.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/janyoungjin/localData/main/data.csv")
 knn = KNeighborsClassifier()
-knn.fit(df[['경도','위도']], df['지역'])
+knn.fit(df[['경도','위도']], df['지역명'])
 
 @app.get("/")
 def  index():
-    return FileResponse("index.html")
+    return HTMLResponse(f"<center><h1>각 지역별 경도와 위도</h1>{df.to_html(index=False,escape=False)}</center>")
 # 경도가 x, 위도가 y
 @app.post("/getlocal")
 def get_result(x: str = Form(...), y: str = Form(...)):
